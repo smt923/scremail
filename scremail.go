d1e38ac6ad11c1d933d241d549f0795ee4113d1f
smt923 <smtea923@gmail.com>
2017-07-14 20:41:05 +0100

    minor cleaning

--- scremail.go
+++ scremail.go
@@ -11,28 +11,25 @@ import (
 	kingpin "gopkg.in/alecthomas/kingpin.v2"
 )
 
-func main() {
-	reg := regexp.MustCompile(`\w+@[\w.-]+|\{(?:\w+, *)+\w+\}@[\w.-]+`)
+var (
+	reg = regexp.MustCompile(`\w+@[\w.-]+|\{(?:\w+, *)+\w+\}@[\w.-]+`)
 
-	urlArg := kingpin.Arg("url", "URL of website.").Required().String()
-	usernameFlag := kingpin.Flag("username", "Output just usernames instead of full emails.").Short('u').Bool()
-	domainFlag := kingpin.Flag("domain", "Output just domains instead of full emails.").Short('d').Bool()
+	urlArg       = kingpin.Arg("url", "URL of website.").Required().String()
+	usernameFlag = kingpin.Flag("username", "Output just usernames instead of full emails.").Short('u').Bool()
+	domainFlag   = kingpin.Flag("domain", "Output just domains instead of full emails.").Short('d').Bool()
+	threadsflag  = kingpin.Flag("threads", "Number of threads to use, default: 1.").Short('t').Default("1").Int()
+)
+
+func main() {
 	kingpin.CommandLine.HelpFlag.Short('h')
 	kingpin.Parse()
 
-	resp, err := http.Get(*urlArg)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error connecting to URL\n")
-		panic(err)
-	}
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error reading body of html request\n")
-		panic(err)
-	}
+	body := reqURL(*urlArg)
 
-	results := reg.FindAllString(string(body), -1)
+	outputResults(reg.FindAllString(string(body), -1))
+}
 
+func outputResults(results []string) {
 	if *domainFlag {
 		domains := make([]string, len(results))
 		for _, email := range results {
@@ -58,16 +55,30 @@ func main() {
 	}
 }
 
-func uniq(elements []string) []string {
+func reqURL(url string) []byte {
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error connecting to URL\n")
+		panic(err)
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading body of html request\n")
+		panic(err)
+	}
+	return body
+}
+
+func uniq(elements []string) (result []string) {
 	encountered := map[string]bool{}
 	// Create a map of all unique elements.
 	for v := range elements {
 		encountered[elements[v]] = true
 	}
 	// Place all keys from the map into a slice.
-	result := []string{}
+	result = []string{}
 	for key, _ := range encountered {
 		result = append(result, key)
 	}
-	return result
+	return
 }
